4e53a1db2d87ca16c2772fe3cec6f51c033869d9
nathro <nathro.software@gmail.com>
2022-01-01 16:44:45 -0800

    Move to using target directory instead of absolute paths

--- src/test/java/autotransform/input/CachedFileTest.java
+++ src/test/java/autotransform/input/CachedFileTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +14,7 @@ import org.junit.jupiter.api.Test;
  * Unit test for CachedFile.
  */
 public class CachedFileTest {
-    private static final String C_TEST_FILE = "C:\\repos\\autotransform\\src\\test\\java\\autotransform\\input\\data\\CachedFileTestData";
+    private static final String C_TEST_FILE = "/test-classes/autotransform/input/data/CachedFileTestData";
     private static final String C_TEST_FILE_CONTENT = "Just some text\nThat is a test";
 
     /**
@@ -19,7 +22,15 @@ public class CachedFileTest {
      */
     @Test
     void testGetFiles() {
-        CachedFile file = new CachedFile(C_TEST_FILE);
+        Path targetPath;
+        try {
+            targetPath = Paths.get(getClass().getResource("/").toURI()).getParent();
+        } catch (URISyntaxException e1) {
+            fail("Unable to find target path");
+            return;
+        }
+
+        CachedFile file = new CachedFile(targetPath.toString() + C_TEST_FILE);
         try {
             assertEquals(C_TEST_FILE_CONTENT, file.getContent());
         } catch (IOException e) {
--- src/test/java/autotransform/input/DirectoryInputTest.java
+++ src/test/java/autotransform/input/DirectoryInputTest.java
@@ -1,25 +1,37 @@
 package autotransform.input;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for directory input.
  */
 class DirectoryInputTest {
 
-    private static final String C_TEST_DATA_DIR = "C:\\repos\\autotransform\\src\\test\\java\\autotransform\\input\\data\\";
+    private static final String C_TEST_DATA_DIR = "\\test-classes\\autotransform\\input\\data\\";
 
     /**
      * Tests that the getFiles function returns expected results.
      */
     @Test
     void testGetFiles() {
-        DirectoryInput input = new DirectoryInput(C_TEST_DATA_DIR + "emptyDirectory");
+        Path targetPath;
+        try {
+            targetPath = Paths.get(getClass().getResource("/").toURI()).getParent();
+        } catch (URISyntaxException e1) {
+            fail("Unable to find target path");
+            return;
+        }
+        DirectoryInput input = new DirectoryInput(targetPath + C_TEST_DATA_DIR + "emptyDirectory");
         assertArrayEquals(new String[0], input.getFiles().toArray());
-        input = new DirectoryInput(C_TEST_DATA_DIR + "nonEmptyDirectory");
-        assertArrayEquals(new String[]{C_TEST_DATA_DIR + "nonEmptyDirectory\\test.txt"}, input.getFiles().toArray());
+        input = new DirectoryInput(targetPath + C_TEST_DATA_DIR + "nonEmptyDirectory");
+        assertArrayEquals(new String[]{targetPath + C_TEST_DATA_DIR + "nonEmptyDirectory\\test.txt"}, input.getFiles().toArray());
     }
 
 }
