0fc0ac4f0bf5e72b5f83724e31f36f0c99ca66b5
nathro <nathro.software@gmail.com>
2022-01-01 16:50:03 -0800

    Rename app to AutoTransform

---
+++ src/main/java/autotransform/AutoTransform.java
@@ -0,0 +1,15 @@
+package autotransform;
+
+/**
+ * Hello world!
+ */
+public final class AutoTransform {
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        
+    }
+}
