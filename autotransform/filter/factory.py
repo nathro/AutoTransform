dc41cb6cc6d46c35e668404910bacbed8ce14ed0
nathro <nathro.software@gmail.com>
2022-04-20 19:18:41 -0700

    More misc cleanup

--- autotransform/filter/factory.py
+++ autotransform/filter/factory.py
@@ -1,7 +1,7 @@
 # AutoTransform
 # Large scale, component based code modification library
 #
-# Licensed under the MIT License <http://opensource.org/licenses/MIT
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2022-present Nathan Rockenbach <http://github.com/nathro>
 
--- tests/runner/__init__.py
+++ tests/runner/__init__.py
@@ -1,6 +1,8 @@
 # AutoTransform
 # Large scale, component based code modification library
 #
-# Licensed under the MIT License <http://opensource.org/licenses/MIT
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2022-present Nathan Rockenbach <http://github.com/nathro>
+
+"""Tests for Runner components."""
