afc878cd58dbb726596745b83df5ebc81d16d568
nathro <nathro.software@gmail.com>
2022-05-26 14:36:38 -0700

    Release 0.2.2a2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Nathan Rockenbach"
 author = "Nathan Rockenbach"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.2a1"
+release = "0.2.2a2"
 
 
 # -- General configuration ---------------------------------------------------
