e1353b29079a6d89326c8877c4ef1d8f29c81d9c
nathro <nathro.software@gmail.com>
2022-06-03 10:09:21 -0700

    Release 0.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Nathan Rockenbach"
 author = "Nathan Rockenbach"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.0a2"
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
