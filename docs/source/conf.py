0519f21a83f62936fee1e93865b591791ff6b487
nathro <nathro.software@gmail.com>
2022-08-03 13:42:18 -0700

    Release 1.0.1 - Alpha 4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Nathan Rockenbach"
 author = "Nathan Rockenbach"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.1a3"
+release = "1.0.1a4"
 
 
 # -- General configuration ---------------------------------------------------
