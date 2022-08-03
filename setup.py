4d0610adea009d86bdabf0e72dcc5c91018ff6bf
nathro <nathro.software@gmail.com>
2022-08-03 13:16:13 -0700

    Release 1.0.1 - Alpha 3

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def generate_datafiles() -> List[Tuple[str, List[str]]]:
 
 setuptools.setup(
     name="AutoTransform",
-    version="1.0.1a2",
+    version="1.0.1a3",
     author="Nathan Rockenbach",
     author_email="nathro.software@gmail.com",
     description="A component based framework for designing automated code modification",
