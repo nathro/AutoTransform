eb5f1a1e63e29ab231002dbdce0ee6ef0c2e8e0e
nathro <nathro.software@gmail.com>
2022-09-23 20:53:33 -0700

    Fix docker stuff after testing on ATTest repo

--- examples/docker/docker_autotransform.sh
+++ examples/docker/docker_autotransform.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [[$1 == "manage" || $1 == "schedule"]]; then
+if [[ $1 == "manage" || $1 == "schedule" ]]; then
     autotransform $1 -v
     RESULT=$?
 fi
 
-if [[$1 == "run"]]; then
+if [[ $1 == "run" ]]; then
     options="-v --name"
     if [[ ! -z "${MAX_SUBMISSIONS}" ]]; then
         options="$options --max-submissions $MAX_SUBMISSIONS"
@@ -17,16 +17,16 @@ if [[$1 == "run"]]; then
     RESULT=$?
 fi
 
-if [[$1 == "update"]]; then
+if [[ $1 == "update" ]]; then
     autotransform $1 -v -e AUTO_TRANSFORM_CHANGE
     RESULT=$?
 fi
 
-if [[-z $RESULT]]; then
+if [[ -z $RESULT ]]; then
     echo "Unknown command $1"
     exit 1
 fi
-if [[$RESULT -ne 0]]; then
+if [[ $RESULT -ne 0 ]]; then
     exit 1
 fi
 exit 0
--- examples/docker/entrypoint.sh
+++ examples/docker/entrypoint.sh
@@ -3,42 +3,45 @@
 docker build -f autotransform/Dockerfile \
     --build-arg REPO_DIR=$REPO_DIR \
     --build-arg COMMAND=$COMMAND \
-    -t autotransform
+    -t autotransform \
+    --no-cache \
+    autotransform
 
-if [[$COMMAND == "schedule" || $COMMAND == "manage"]]; then
+if [[ $COMMAND == "schedule" || $COMMAND == "manage" ]]; then
     docker run -e AUTO_TRANSFORM_CONFIG=environment \
         -e AUTO_TRANSFORM_GITHUB_TOKEN="$GITHUB_TOKEN" \
-        -v "$(pwd)"/"$REPO_DIR":/$REPO_DIR \
+        -v "$(pwd)":/$REPO_DIR \
         autotransform
     RESULT=$?
 fi
 
-if [[$COMMAND == "run"]]; then
+if [[ $COMMAND == "run" ]]; then
     docker run -e AUTO_TRANSFORM_CONFIG=environment \
         -e AUTO_TRANSFORM_GITHUB_TOKEN="$GITHUB_TOKEN" \
         -e FILTER="$FILTER" \
-        -e MAX_SUBMISSIONS="$MAX_SUBMISSIONS"
-        -v "$(pwd)"/"$REPO_DIR":/$REPO_DIR \
+        -e MAX_SUBMISSIONS="$MAX_SUBMISSIONS" \
+        -e SCHEMA_NAME="$SCHEMA_NAME" \
+        -v "$(pwd)":/$REPO_DIR \
         autotransform
     RESULT=$?
 fi
 
-if [[$COMMAND == "update"]]; then
+if [[ $COMMAND == "update" ]]; then
     docker run -e AUTO_TRANSFORM_CONFIG=environment \
         -e AUTO_TRANSFORM_GITHUB_TOKEN="$GITHUB_TOKEN" \
         -e AUTO_TRANSFORM_CHANGE="$AUTO_TRANSFORM_CHANGE" \
-        -v "$(pwd)"/"$REPO_DIR":/$REPO_DIR \
+        -v "$(pwd)":/$REPO_DIR \
         autotransform
     RESULT=$?
 fi
 
 docker system prune -a -f >/dev/null 2>&1 &
 
-if [[-z $RESULT]]; then
+if [[ -z $RESULT ]]; then
     echo "Unknown command $1"
     exit 1
 fi
-if [[$RESULT -ne 0]]; then
+if [[ $RESULT -ne 0 ]]; then
     exit 1
 fi
 exit 0
