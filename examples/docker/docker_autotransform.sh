6250e9787a86d68919a71eae5531fe36fb9e73e9
nathro <nathro.software@gmail.com>
2022-10-05 03:31:00 -0700

    Simplify Docker associated files and scripts

--- examples/docker/docker_autotransform.sh
+++ examples/docker/docker_autotransform.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 
-if [[ $1 == "manage" || $1 == "schedule" ]]; then
-    autotransform $1 -v
-    RESULT=$?
-fi
+options="-v"
 
-if [[ $1 == "run" ]]; then
-    options="-v --name"
+if [[ $COMMAND == "run" ]]; then
+    options="$options --name"
+
     if [[ ! -z "${MAX_SUBMISSIONS}" ]]; then
         options="$options --max-submissions $MAX_SUBMISSIONS"
     fi
+
     if [[ ! -z "${FILTER}" ]]; then
         options="$options --filter '$FILTER'"
     fi
-    autotransform $1 ${options} "${SCHEMA_NAME}"
-    RESULT=$?
+
+    options="$options \"$SCHEMA_NAME\""
 fi
 
 if [[ $1 == "update" ]]; then
-    autotransform $1 -v -e AUTO_TRANSFORM_CHANGE
-    RESULT=$?
+    echo "Change: $AUTO_TRANSFORM_CHANGE"
+    options="$options -e AUTO_TRANSFORM_CHANGE"
 fi
 
-if [[ -z $RESULT ]]; then
-    echo "Unknown command $1"
-    exit 1
-fi
+echo "Running autotransform $COMMAND $options"
+autotransform $COMMAND $options
+RESULT=$?
+
 if [[ $RESULT -ne 0 ]]; then
+    echo "Failed to execute AutoTransform"
     exit 1
 fi
+
 exit 0
