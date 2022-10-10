9469d5174720b596e8e9b1c46197769ac850d462
nathro <nathro.software@gmail.com>
2022-10-10 09:32:45 -0700

    Release 1.0.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Nathan Rockenbach"
 author = "Nathan Rockenbach"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.5a4"
+release = "1.0.5"
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def generate_datafiles() -> List[Tuple[str, List[str]]]:
 
 setuptools.setup(
     name="AutoTransform",
-    version="1.0.5a4",
+    version="1.0.5",
     author="Nathan Rockenbach",
     author_email="nathro.software@gmail.com",
     description="A component based framework for designing automated code modification",
