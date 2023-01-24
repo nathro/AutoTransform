f7cab470710ab6538cc48a92efc6a15f2f150666
nathro <nathro.software@gmail.com>
2023-01-24 10:55:32 -0800

    Release 1.1.0 - Alpha 6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Nathan Rockenbach"
 author = "Nathan Rockenbach"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.0a5"
+release = "1.1.0a6"
 
 
 # -- General configuration ---------------------------------------------------
