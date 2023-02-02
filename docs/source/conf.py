be7133b329f539255aa360895da01d3b1fe8b288
nathro <nathro.software@gmail.com>
2023-02-01 16:22:21 -0800

    Release 1.1.0 - Alpha 13

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Nathan Rockenbach"
 author = "Nathan Rockenbach"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.0a12"
+release = "1.1.0a13"
 
 
 # -- General configuration ---------------------------------------------------
