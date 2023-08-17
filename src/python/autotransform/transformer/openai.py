4dfb5050379b952b977f1fb5413009a9fed6a00e
nathro <nathro.software@gmail.com>
2023-08-16 17:21:39 -0700

    Add Validator member to OpenAITransformer

--- src/python/autotransform/transformer/openai.py
+++ src/python/autotransform/transformer/openai.py
@@ -11,7 +11,7 @@
 
 from __future__ import annotations
 
-from typing import ClassVar, Optional
+from typing import Any, ClassVar, Dict, List, Optional, Type
 
 import openai  # pylint: disable=import-error
 from autotransform.config import get_config
@@ -19,6 +19,9 @@ from autotransform.item.base import Item
 from autotransform.item.file import FileItem
 from autotransform.transformer.base import TransformerName
 from autotransform.transformer.single import SingleTransformer
+from autotransform.validator.base import FACTORY as validator_factory
+from autotransform.validator.base import Validator
+from pydantic import Field
 
 
 class OpenAITransformer(SingleTransformer):
@@ -33,6 +36,8 @@ class OpenAITransformer(SingleTransformer):
         system_message (optional, Optional[str]): The system message to use. Defaults to None.
         temperature (optional, float): The temperature to use to control the quality of outputs.
             Defaults to 0.4.
+        validators (optional, List[Validator]): A set of validators to use to provide feedback to
+            the LLM with issues it may have produced. Defaults to an empty list.
         name (ClassVar[TransformerName]): The name of the component.
     """
 
@@ -40,6 +45,7 @@ class OpenAITransformer(SingleTransformer):
     model: str = "gpt-3.5-turbo"
     system_message: Optional[str] = None
     temperature: float = 0.4
+    validators: List[Validator] = Field(default_factory=list)
 
     name: ClassVar[TransformerName] = TransformerName.OPEN_AI
 
@@ -58,19 +64,21 @@ class OpenAITransformer(SingleTransformer):
         # Set up messages for prompt
         if self.system_message:
             messages.append({"role": "system", "content": self.system_message})
-        messages.append({
-            "role": "user",
-            "content": self._replace_sentinel_values(self.prompt, item),
-        })
+        messages.append(
+            {
+                "role": "user",
+                "content": self._replace_sentinel_values(self.prompt, item),
+            }
+        )
         # Get completition
         chat_completion = openai.ChatCompletion.create(
-            model=self.model,
-            messages=messages,
-            temperature=self.temperature
+            model=self.model, messages=messages, temperature=self.temperature,
         )
-        item.write_content(self._extract_code_from_completion(
-            chat_completion.choices[0].message.content,
-        ))
+        item.write_content(
+            self._extract_code_from_completion(
+                chat_completion.choices[0].message.content,
+            )
+        )
 
     def _replace_sentinel_values(self, prompt: str, item: FileItem) -> str:
         """Replaces sentinel values in a prompt
@@ -112,3 +120,47 @@ class OpenAITransformer(SingleTransformer):
         else:
             code = "\n".join(code_lines)
         return code
+
+    @classmethod
+    def from_data(cls: Type[OpenAITransformer], data: Dict[str, Any]) -> OpenAITransformer:
+        """Produces an instance of the component from decoded data.
+
+        Args:
+            data (Mapping[str, Any]): The JSON decoded data.
+
+        Returns:
+            OpenAITransformer: An instance of the component.
+        """
+
+        prompt = data["propt"]
+        assert isinstance(prompt, str)
+        if "model" in data:
+            model = data["model"]
+            assert isinstance(model, str)
+        else:
+            model = "gpt-3.5-turbo"
+        if "system_message" in data:
+            system_message = data["system_message"]
+            if system_message is not None:
+                assert isinstance(system_message, str)
+        else:
+            system_message = None
+        if "temperature" in data:
+            temperature = data["temperature"]
+            assert isinstance(temperature, float)
+        else:
+            temperature = 0.4
+        if "validators" in data:
+            validators = [
+                validator_factory.get_instance(validator) for validator in data["validators"]
+            ]
+        else:
+            validators = []
+
+        return cls(
+            prompt=prompt,
+            model=model,
+            system_message=system_message,
+            temperature=temperature,
+            validators=validators,
+        )
