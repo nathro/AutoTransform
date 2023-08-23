4d9da728f8cce9ca47799f2f3b3d07e1d4f740db
runner <runner@fv-az409-839>
2023-08-23 09:02:53 +0000

    [AutoTransform][Crawler] Crawl python files: tests/item

--- tests/item/test_factory.py
+++ tests/item/test_factory.py
@@ -18,15 +18,12 @@ def test_all_enum_values_present():
     """Ensures that all values from the enum are present in the factory map,
     and only enum values are present."""
 
-    missing_values = [
-        item_name for item_name in ItemName if item_name not in FACTORY.get_components()
-    ]
-    assert not missing_values, "Names missing from factory: " + ", ".join(missing_values)
+    factory_components = FACTORY.get_components()
+    missing_values = [item_name for item_name in ItemName if item_name not in factory_components]
+    assert not missing_values, f"Names missing from factory: {', '.join(missing_values)}"
 
-    extra_values = [
-        item_name for item_name in FACTORY.get_components() if item_name not in ItemName
-    ]
-    assert not extra_values, "Extra names in factory: " + ", ".join(extra_values)
+    extra_values = [item_name for item_name in factory_components if item_name not in ItemName]
+    assert not extra_values, f"Extra names in factory: {', '.join(extra_values)}"
 
 
 def test_fetching_components():
@@ -63,9 +60,8 @@ def test_encoding_and_decoding() -> None:
         assert name in test_components, f"No test components for Item {name}"
 
     for name, components in test_components.items():
-        assert name in ItemName, f"{name} is not a valid ItemName"
         for component in components:
-            component_dict = {"name": name} | component
+            component_dict = {"name": name, **component}
             component_instance = FACTORY.get_instance(component_dict)
             assert (
                 component_instance.name == name
